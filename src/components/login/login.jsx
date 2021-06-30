a4747d4f8dab90ae841915490147dd8e34629899
min woo <alsdn9342@gmail.com>
2021-06-30 20:26:47 +0900

    Add Router to navigate to Maker component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header'
 import styles from './login.module.css'
 
 const Login = ({authService}) => {
+    const history = useHistory();
 
+    const goToMaker = userId => {
+        history.push({
+            pathname:'/maker',
+            state: { id:userId} // insert uid for each login method, Google and Github.
+        });
+    };
     const onLogin = event => {
        authService
        .login(event.currentTarget.textContent)
-       .then(console.log);
-    }
+       .then(data => goToMaker(data.user.uid));
+    };
+
+    useEffect(() => {
+        authService.onAuthChange(user => {
+            user && goToMaker(user.uid);
+        })
+
+    });
     return (
-        <section className={styles.login}>
+       
+         <section className={styles.login}>
             <Header />
             <section>
                 <h1>Login</h1>
@@ -26,6 +42,7 @@ const Login = ({authService}) => {
             </section>
             <Footer />
         </section>
+    
     );
 };
 
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -7,6 +7,18 @@ class AuthService {
       const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
       return firebaseApp.auth().signInWithPopup(authProvider);
     }
+
+    logout() {
+      firebase.auth().signOut();
+    }
+
+    //Check if user is signed in or not. 
+    onAuthChange(onUserChanged){
+      firebase.auth().onAuthStateChanged(user => {
+        onUserChanged(user);
+      })
+
+    }
 }
 
 export default AuthService;
